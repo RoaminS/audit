7d21076b167009601f53f2bd4c75a3ee8da177bd
Romain <consulting@web3-crypto.xyz>
2025-06-13 13:02:08 +0200

    __init__.py

--- src/hebbian_learning/__init__.py
+++ src/hebbian_learning/__init__.py
@@ -1,1 +1,9 @@
+# hebbian_learning/__init__.py
 
+# This file makes the 'hebbian_learning' directory a Python package.
+# It's good practice to import core components here for easier access.
+
+from .hebbian_neuron import HebbianNeuron
+from .hebbian_network import HebbianNetwork
+from .central_memory import CentralMemory
+from .feedback_interpreter import FeedbackInterpreter
