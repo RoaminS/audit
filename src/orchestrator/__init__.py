95f161fc2bf0a46ba049d5e17ef9dcedcda48979
Romain <consulting@web3-crypto.xyz>
2025-06-13 13:10:23 +0200

    __init__.py

--- src/orchestrator/__init__.py
+++ src/orchestrator/__init__.py
@@ -1,1 +1,5 @@
+# orchestrator/__init__.py
 
+# This file makes the 'orchestrator' directory a Python package.
+# You can import core components here for easier access if needed.
+from .attack_orchestrator import AttackOrchestrator
